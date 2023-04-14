8871cbbf81a32c983e97a77f75339e04ecf73075
413C5 <a_gonzalezzarate2@yahoo.com>
2023-04-13 18:37:44 -0600

    MongoDB
    Se hace el uso de MongoDB y el manejo de base de datos con el mismo

--- index.js
+++ index.js
@@ -1,32 +1,11 @@
-//const http = require('http')
-const { response } = require('express')
+require('dotenv').config()
+const { request,response } = require('express')
 const express = require('express')
 const app = express()
 const cors = require('cors')
-
+const Note=require('./models/note')
 app.use(cors())
 app.use(express.json())
-
-let notes = [
-    {
-        "id": 1,
-        "content": "HTML is Easy",
-        "date": "2023-02-14",
-        "important": false
-    },
-    {
-        "id": 2,
-        "content": "Browser can execute only JavaScript",
-        "date": "2023-02-15",
-        "important": true
-    },
-    {
-        "id": 3,
-        "content": "GET and POST are the most important methods of HTTP protocol",
-        "date": "2023-02-15",
-        "important": false
-    }
-]
 
 const requestLogger = (request, response, next) => {
     console.log('Method:', request.method)
@@ -43,58 +22,39 @@ app.get('/', (request, response) => {
 })
 
 app.get('/api/notes', (request, response) => {
-    response.json(notes)
+    Note.find({}).then(notes=>{
+        response.json(notes)
+    })
 })
 
+//Modificacion extra
 app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    console.log('id=', id)
-    const note = notes.find(x => x.id === id)
-    console.log(note)
+    Note.findById(request.params.id)
+      .then(note => {
+        if (note) {
+          response.json(note)
+        } else {
+          response.status(404).end() 
+        }
+      })
+      .catch(error => {
+        console.log(error)
+        response.status(400).send({ error: 'malformatted id' })
+      })
+  })
 
-    //SI es un objeto
-    if (note) {
-        response.json(note)
-    }
-    //Es nulo
-    else {
-        response.status(404).send()
-    }
-})
 
+//Modificacion extra
 app.delete('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    let size = notes.length
-    notes = notes.filter(x => x.id !== id)
-
-    //Si mi tamaño anterior es mayor que el actual
-    if (size > notes.length) {
+    Note.findByIdAndRemove(request.params.id)
+      .then(result => {
         response.status(204).send()
-    }
-
-    else {
+      })
+      .catch(error =>{
+        console.log(error)
         response.status(404).send()
-    }
-
-})
-
-const generateId = () => {
-    const maxId = (() => {
-        if (notes.length > 0) {
-            return Math.max(...notes.map(x => x.id))
-        }
-        else {
-            return 0
-        }
-    })()
-
-    return maxId + 1
-
-    /* const maxId=notes.length>0
-      ?Math.max(...notes.map(x=>x.id))
-      :0
-    return maxId+1 */
-}
+      })
+  })
 
 app.post('/api/notes', (request, response) => {
     //Tiene que tener la sintaxis del objeto
@@ -105,41 +65,35 @@ app.post('/api/notes', (request, response) => {
             error: 'content missing'
         })
     }
-    const note = {
+    const note = Note({
         content: body.content,
         important: body.important || false,
-        date: new Date(),
-        id: generateId()
-
-    }
-    notes = notes.concat(note)
-    response.json(note)
+        date: new Date()
+    })
+    note.save().then(savedNote=>{
+        response.json(savedNote)
+    })
 })
 
+//Modificacion extra
 app.put('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    console.log('id=', id)
-    const note = notes.find(x => x.id === id)
     const body = request.body
+  
     const noteUpdate = {
-        content: body.content,
-        important: body.important,
-        date: body.date,
-        id: body.id
-
+      content: body.content,
+      important: body.important,
+      /* date: body.date */
     }
-    console.log(note)
-
-    //SI es un objeto
-    if (note) {
-        notes = notes.map(x => x.id !== id ? x : noteUpdate)
-        response.json(noteUpdate)
-    }
-    //Es nulo
-    else {
+  
+    Note.findByIdAndUpdate(request.params.id, noteUpdate, { new: true })
+      .then(updatedNote => {
+        response.json(updatedNote)
+      })
+      .catch(error=>{
+        console.log(error)
         response.status(404).send()
-    }
-})
+      })
+  })
 
 const unknownPath = (request, response) => {
     response.status(404).json({
@@ -150,8 +104,9 @@ const unknownPath = (request, response) => {
 app.use(unknownPath)
 
 //En que puerto se atendera el servidor
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 
+//
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
---
+++ mongo.js
@@ -0,0 +1,49 @@
+//mongodb+srv://fullstack:<password>@cluster0.ezwn4oa.mongodb.net/?retryWrites=true&w=majority
+
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+    console.log('ERROR, need provide password : node mongo.js <password>')
+    process.exit(1)
+}
+
+if (process.argv.length < 4) {
+    console.log('ERROR, need provide message : node mongo.js <password> <message>')
+    process.exit(1)
+}
+
+const password = process.argv[2]
+const message=process.argv[3]
+
+const url = `mongodb+srv://fullstack:${password}@cluster0.ezwn4oa.mongodb.net/app-note?retryWrites=true&w=majority`
+
+mongoose.connect(url)
+
+const noteSchema = new mongoose.Schema({
+    content:String,
+    date:Date,
+    important:Boolean
+})
+
+const Note= mongoose.model('Note',noteSchema)
+
+const note=new Note({
+    content:message,
+    date:new Date(),
+    important:true
+})
+
+//Insert
+/* note.save().then(result=>{
+    console.log('note saved!')
+    console.log(result)
+    mongoose.connection.close()
+}) */
+
+//Select
+Note.find({important:false}).then(result=>{
+    result.forEach(note => {
+        console.log(note)
+    })
+    mongoose.connection.close()
+})
