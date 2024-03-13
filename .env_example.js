2b7842702788f91f11a40873245fb8c761eaccf1
413C5 <a_gonzalezzarate2@yahoo.com>
2024-03-12 18:29:01 -0600

    Build included

---
+++ .env_example.js
@@ -0,0 +1,5 @@
+MONGODB_URI='mongodb+srv://<username>:<password>@cluster0.ezwn4oa.mongodb.net/<app-name>?retryWrites=true&w=majority'
+
+PORT=3001
+
+TEST_MONGODB_URI=mongodb+srv://<username>:<password>@cluster0-ostce.mongodb.net/<app-name-test>?retryWrites=true
